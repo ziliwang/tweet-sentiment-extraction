e621a89ac5a402f9f32d8e778ab188378e27b8ec
Zili Wang <wzlnot@gmail.com>
2020-04-24 09:50:11 +0800

    note: 1. XLNET head add, local cv improve to 7153

--- code/predict.py
+++ code/predict.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from transformers import *
+from transformers.modeling_utils import PoolerAnswerClass, PoolerEndLogits, PoolerStartLogits
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split, KFold
@@ -14,6 +15,7 @@ from tqdm import tqdm
 from copy import deepcopy
 from tokenizers import ByteLevelBPETokenizer
 import pandas as pd
+import collections
 
 
 MAX_LEN = 200
@@ -58,45 +60,67 @@ class RobertaForQuestionAnswering(BertPreTrainedModel):
         super(RobertaForQuestionAnswering, self).__init__(config)
         self.roberta = RobertaModel(config)
         self.logits = nn.Linear(config.hidden_size*2, 2)
-        self.dropout = nn.Dropout(0.5)
+        self.start_logits = PoolerStartLogits(config)
+        self.end_logits = PoolerEndLogits(config)
+        self.start_n_top = 5
+        self.end_n_top = 5
+        # nn.init.xavier_uniform_(self.logits.weight)
         self.init_weights()
+        # torch.nn.init.normal_(self.logits.weight, std=0.02)
 
     def forward(self, input_ids, attention_mask=None, start_positions=None, end_positions=None):
         outputs = self.roberta(input_ids, attention_mask=attention_mask)
-        hidden_states = torch.cat([outputs[0], outputs[1][:,None,:].expand_as(outputs[0])], dim=-1)
-        start_end_logits = self.logits(self.dropout(hidden_states))
-        start_logits, end_logits = start_end_logits.split(1, dim=-1)
+        hidden_states = outputs[0]
+        p_mask = 1 - attention_mask.float() * (input_ids != sep_token_id).float()
+        start_logits = self.start_logits(hidden_states, p_mask=p_mask)
 
         if start_positions is not None and end_positions is not None:
             for x in (start_positions, end_positions):
                 if x.dim() > 1:
                     x.squeeze_(-1)
+            end_logits = self.end_logits(hidden_states, start_positions=start_positions, p_mask=p_mask)
             start_loss = F.cross_entropy(start_logits.squeeze(-1), start_positions, reduction='none')
             end_loss = F.cross_entropy(end_logits.squeeze(-1), end_positions, reduction='none')
             return start_loss + end_loss
 
         if not self.training:
-            p_mask = attention_mask.float()  # 1 for available 0 for unavailable
-            p_mask[:, :3] = 0.0
-            start_logits = start_logits.squeeze(-1) * p_mask - 1e30 * (1 - p_mask)
-            end_logits = end_logits.squeeze(-1) * p_mask - 1e30 * (1- p_mask)
-            return start_logits, end_logits
+            bsz, slen, hsz = hidden_states.size()
+            start_log_probs = start_logits.softmax(-1).log()  # shape (bsz, slen)
+
+            start_top_log_probs, start_top_index = torch.topk(start_log_probs, self.start_n_top, dim=-1)  # shape (bsz, start_n_top)
+            # start_top_log_probs = start_top_log_probs - start_log_probs[:, 0:1].expand(-1, self.start_n_top)
+            start_top_index_exp = start_top_index.unsqueeze(-1).expand(-1, -1, hsz)  # shape (bsz, start_n_top, hsz)
+            start_states = torch.gather(hidden_states, -2, start_top_index_exp)  # shape (bsz, start_n_top, hsz)
+            start_states = start_states.unsqueeze(1).expand(-1, slen, -1, -1)  # shape (bsz, slen, start_n_top, hsz)
+
+            hidden_states_expanded = hidden_states.unsqueeze(2).expand_as(start_states)  # shape (bsz, slen, start_n_top, hsz)
+            p_mask = p_mask.unsqueeze(-1) if p_mask is not None else None
+            end_logits = self.end_logits(hidden_states_expanded, start_states=start_states, p_mask=p_mask)
+            end_log_probs = F.softmax(end_logits, dim=1).log()  # shape (bsz, slen, start_n_top)
+
+            end_top_log_probs, end_top_index = torch.topk(end_log_probs, self.end_n_top, dim=1)  # shape (bsz, end_n_top, start_n_top)
+            # end_top_log_probs = end_top_log_probs - end_log_probs[:, 0:1, :].expand(-1, self.end_n_top, -1)
+            end_top_log_probs = end_top_log_probs.view(-1, self.start_n_top * self.end_n_top)
+            end_top_index = end_top_index.view(-1, self.start_n_top * self.end_n_top)
+
+            return start_top_log_probs, start_top_index, end_top_log_probs, end_top_index
 
 
-def pridect_epoch(model, dataiter, starts_logits_5cv, ends_logits_5cv):
+def pridect_epoch(model, dataiter, bagging_cache):
     model.eval()
     with torch.no_grad():
-        for ids, inputs, _, _ in dataiter:
-            start_logits, end_logits = model(input_ids=inputs, attention_mask=(inputs!=pad_token_id).long())
-            for i, j, k in zip(ids, start_logits, end_logits):
-                if i in starts_logits_5cv:
-                    starts_logits_5cv[i] += j
-                else:
-                    starts_logits_5cv[i] = j
-                if i in ends_logits_5cv:
-                    ends_logits_5cv[i] += k
-                else:
-                    ends_logits_5cv[i] = k
+        for ids, inputs, offsets, texts in dataiter:
+            s_top_probs, s_top_idxs, e_top_probs, e_top_idxs = model(input_ids=inputs, attention_mask=(inputs!=pad_token_id).long())
+            for i in range(inputs.shape[0]):
+                if inputs[i][1] == 7974:
+                    continue
+                for _i, s_idx in enumerate(s_top_idxs[i]):
+                    if s_idx < 3: continue
+                    for _j, e_idx in enumerate(e_top_idxs[i]):
+                        if _j % model.end_n_top != _i: continue
+                        if s_idx <= e_idx and e_idx -3 < len(offsets[i]):
+                            cand_text = texts[i][offsets[i][s_idx-3][0]: offsets[i][e_idx-3][1]]
+                            bagging_cache[ids[i]][cand_text].append((s_top_probs[i][_i]+e_top_probs[i][_j]).detach().cpu().numpy())
 
 
 @click.command()
@@ -114,31 +138,21 @@ def main(test_path, vocab, merges, models, config):
     model = RobertaForQuestionAnswering(model_config).cuda()
     testiter = DataLoader(test, batch_size=32, shuffle=False, collate_fn=collect_func)
     print(f"5cv {saved_models['score']}")
-    starts_logits_5cv = {}
-    ends_logits_5cv = {}
+    bagging_cache = collections.defaultdict(lambda: collections.defaultdict(list))
     for state_dict in saved_models['models']:
         model.load_state_dict(state_dict)
-        pridect_epoch(model, testiter, starts_logits_5cv, ends_logits_5cv)
+        pridect_epoch(model, testiter, bagging_cache)
     submit = pd.DataFrame()
     submit['textID'] = test_df['textID']
     submit['selected_text'] = test_df['text']
-    id2sentiment = dict((r.textID, r.sentiment) for _, r in test_df.iterrows())
-    for ids, _, offsets, texts in testiter:
-        for id, offset, text in zip(ids, offsets, texts):
-            if id2sentiment[id] == 'neutral':
-                continue
-            start = end = None
-            for i_s in torch.argsort(starts_logits_5cv[id], descending=True):
-                if i_s > 1 and i_s < len(offset)+3:
-                    start = i_s
-                    break
-            for i_e in torch.argsort(ends_logits_5cv[id], descending=True):
-                if i_e >= start and i_e < len(offset)+3:
-                    end = i_e
-                    break
-            assert start is not None
-            assert end is not None
-            submit.selected_text[submit.textID == id] = text[offset[start-3][0]: offset[end-3][1]]
+
+    ans_mapping = {}
+
+    for item_id in bagging_cache:
+        ans, logprob = sorted(bagging_cache[item_id].items(), key=lambda x: np.mean(x[1]))[-1]
+        ans_mapping[item_id] = ans
+
+    submit['selected_text'] = submit.apply(lambda x: ans_mapping.get(x.textID, x.selected_text), axis=1)
     submit.to_csv("submission.csv", index=False)
 
 
--- code/train.py
+++ code/train.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from transformers import *
+from transformers.modeling_utils import PoolerAnswerClass, PoolerEndLogits, PoolerStartLogits
 from transformers.modeling_bert import *
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
@@ -99,6 +100,31 @@ def validate_epoch(model, dataiter):
     return score/sample_counts
 
 
+def validate_epoch1(model, dataiter):
+    model.eval()
+    score = 0
+    sample_counts = 0
+    with torch.no_grad():
+        for inputs, _, _, offsets, texts, gts in dataiter:
+            s_top_probs, s_top_idxs, e_top_probs, e_top_idxs = model(input_ids=inputs, attention_mask=(inputs!=pad_token_id).long())
+            batch_size = inputs.shape[0]
+            sample_counts += batch_size
+            for i in range(batch_size):
+                c = []
+                for _i, s_idx in enumerate(s_top_idxs[i]):
+                    if s_idx < 3: continue
+                    for _j, e_idx in enumerate(e_top_idxs[i]):
+                        if _j % model.end_n_top != _i: continue
+                        if s_idx <= e_idx and e_idx -3 < len(offsets[i]):
+                            c.append((s_top_probs[i][_i]+e_top_probs[i][_j], s_idx, e_idx))
+                _, start, end = sorted(c)[-1]
+                predict = texts[i][offsets[i][start-3][0]: offsets[i][end-3][1]]
+                if inputs[i][1] == 7974:
+                    predict = texts[i]
+                score += jaccard(gts[i], predict)
+    return score/sample_counts
+
+
 def deepcopy_state_dict_to_cpu(model):
     output = {}
     for k, v in model.state_dict().items():
@@ -111,7 +137,7 @@ def fold_train(model, optimizer, lr_scheduler, epoch, train_dataiter, val_datait
     best_model = deepcopy_state_dict_to_cpu(model)
     for e in range(epoch):
         loss = train_epoch(model, optimizer, lr_scheduler, train_dataiter, accumulate_step)
-        score = validate_epoch(model, val_dataiter)
+        score = validate_epoch1(model, val_dataiter)
         print(f'epoch {e} loss {loss:.6f} score: {score:.6f}')
         if score > best_score:
             best_score = score
@@ -169,6 +195,57 @@ class RobertaForQuestionAnswering(BertPreTrainedModel):
             return start_logits, end_logits
 
 
+class RobertaForQuestionAnswering1(BertPreTrainedModel):
+    def __init__(self, config):
+        super(RobertaForQuestionAnswering1, self).__init__(config)
+        self.roberta = RobertaModel(config)
+        self.logits = nn.Linear(config.hidden_size*2, 2)
+        self.start_logits = PoolerStartLogits(config)
+        self.end_logits = PoolerEndLogits(config)
+        self.start_n_top = 5
+        self.end_n_top = 5
+        # nn.init.xavier_uniform_(self.logits.weight)
+        self.init_weights()
+        # torch.nn.init.normal_(self.logits.weight, std=0.02)
+
+    def forward(self, input_ids, attention_mask=None, start_positions=None, end_positions=None):
+        outputs = self.roberta(input_ids, attention_mask=attention_mask)
+        hidden_states = outputs[0]
+        p_mask = 1 - attention_mask.float() * (input_ids != sep_token_id).float()
+        start_logits = self.start_logits(hidden_states, p_mask=p_mask)
+
+        if start_positions is not None and end_positions is not None:
+            for x in (start_positions, end_positions):
+                if x.dim() > 1:
+                    x.squeeze_(-1)
+            end_logits = self.end_logits(hidden_states, start_positions=start_positions, p_mask=p_mask)
+            start_loss = F.cross_entropy(start_logits.squeeze(-1), start_positions, reduction='none')
+            end_loss = F.cross_entropy(end_logits.squeeze(-1), end_positions, reduction='none')
+            return start_loss + end_loss
+
+        if not self.training:
+            bsz, slen, hsz = hidden_states.size()
+            start_log_probs = start_logits.softmax(-1).log()  # shape (bsz, slen)
+
+            start_top_log_probs, start_top_index = torch.topk(start_log_probs, self.start_n_top, dim=-1)  # shape (bsz, start_n_top)
+            # start_top_log_probs = start_top_log_probs - start_log_probs[:, 0:1].expand(-1, self.start_n_top)
+            start_top_index_exp = start_top_index.unsqueeze(-1).expand(-1, -1, hsz)  # shape (bsz, start_n_top, hsz)
+            start_states = torch.gather(hidden_states, -2, start_top_index_exp)  # shape (bsz, start_n_top, hsz)
+            start_states = start_states.unsqueeze(1).expand(-1, slen, -1, -1)  # shape (bsz, slen, start_n_top, hsz)
+
+            hidden_states_expanded = hidden_states.unsqueeze(2).expand_as(start_states)  # shape (bsz, slen, start_n_top, hsz)
+            p_mask = p_mask.unsqueeze(-1) if p_mask is not None else None
+            end_logits = self.end_logits(hidden_states_expanded, start_states=start_states, p_mask=p_mask)
+            end_log_probs = F.softmax(end_logits, dim=1).log()  # shape (bsz, slen, start_n_top)
+
+            end_top_log_probs, end_top_index = torch.topk(end_log_probs, self.end_n_top, dim=1)  # shape (bsz, end_n_top, start_n_top)
+            # end_top_log_probs = end_top_log_probs - end_log_probs[:, 0:1, :].expand(-1, self.end_n_top, -1)
+            end_top_log_probs = end_top_log_probs.view(-1, self.start_n_top * self.end_n_top)
+            end_top_index = end_top_index.view(-1, self.start_n_top * self.end_n_top)
+
+            return start_top_log_probs, start_top_index, end_top_log_probs, end_top_index
+
+
 @click.command()
 @click.option('--data', default='roberta.input.joblib')
 @click.option('--pretrained', default='../model/roberta-l12/')
@@ -189,7 +266,7 @@ def main(data, pretrained, lr, batch_size, epoch, accumulate_step, seed):
         train = [data[i] for i in train_idx if data[i]['sentiment'] != 7974]
         # train = [data[i] for i in train_idx]
         val = [data[i] for i in val_idx]
-        model = RobertaForQuestionAnswering.from_pretrained(pretrained).cuda()
+        model = RobertaForQuestionAnswering1.from_pretrained(pretrained).cuda()
         no_decay = ['.bias', 'LayerNorm.bias', 'LayerNorm.weight']
         optimizer = AdamW([{"params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
                             "lr": lr, 'weight_decay': 1e-2},
